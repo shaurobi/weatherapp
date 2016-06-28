7eef36ff4af10392a7a97fe60329fa3ee10d642b
shaurobi <shaurobi@cisco.com>
2016-06-28 21:29:50 +1000

    Removed comments from core-work

--- core-work.py
+++ core-work.py
@@ -8,9 +8,14 @@ web = "http://api.openweathermap.org/data/2.5/weather?"
 key = "83fcd7c8d13fa1ebfa85e29312efa126"
 city = "Sydney"
 
+#build URL request from above variables
 url = web + "q=" + city + "&APPID=" + key + "&units=metric"
 
+#pull response from url
 value = requests.get(url)
+
+#print status code from request -- 200 means it worked!
 print(value.status_code)
+
+#print the main temp
 print(value.json()['main']['temp'])
-test = value.json()
