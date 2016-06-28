6fcb060d4dcf4000043d5ce524c1f63107e969af
shaurobi <shaurobi@cisco.com>
2016-06-28 21:27:57 +1000

    Removed comments from core-work

--- core-work.py
+++ core-work.py
@@ -4,17 +4,6 @@ import sys
 import json
 import requests
 
-my_headers = {'content-type': 'application/json-rpc'}
-# response = requests.post(url, data=my_data, headers=my_headers, auth=(username, password))
-
-
-#kick_start_image = response.json()['result']['body']['kickstart_ver_str']
-#system_image = response.json()['result']['body']['kick_file_name']
-#payload = [{'jsonrpc': '2.0', 'method': 'cli', 'params': ['show system resources',1], 'id': '1'}]
-#my_data = json.dumps(payload)
-#response = requests.post(url, data=my_data, headers=my_headers, auth=(username, password))
-#cpu_state_idle=response.json()['result']['body']['cpu_state_idle']
-
 web = "http://api.openweathermap.org/data/2.5/weather?"
 key = "83fcd7c8d13fa1ebfa85e29312efa126"
 city = "Sydney"
